fabfa0c9f977ef5db97b27c900617e6d79df76b1
tynor88 <tynor@hotmail.com>
2014-03-29 19:17:02 +0100

    Adding sample project for running Quartz job as a service

---
+++ Topshelf.SimpleInjector.QuartzAsService.Sample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Quartz;
+using SimpleInjector;
+
+namespace Topshelf.SimpleInjector.Quartz.Sample
+{
+    class Program
+    {
+        private static readonly Container _container = new Container();
+
+        static void Main(string[] args)
+        {
+            //Register services
+            _container.Register<IJob, WithInjectedDependenciesJob>();
+            _container.Register<IDependencyInjected, DependencyInjected>();
+
+            HostFactory.Run(config =>
+            {
+                config.UseQuartzSimpleInjector(_container);
+
+                //Check container for errors
+                _container.Verify();
+
+                config.ScheduleQuartzJobAsService(configurator =>
+                    configurator.WithJob(
+                        () =>
+                            JobBuilder.Create<WithInjectedDependenciesJob>()
+                                .WithIdentity("WithInjectedDependenciesJob")
+                                .Build())
+                        .AddTrigger(
+                            () =>
+                                TriggerBuilder.Create()
+                                    .WithSimpleSchedule(
+                                        builder =>
+                                            builder.WithIntervalInSeconds(1).RepeatForever()).Build()));
+            });
+        }
+
+        public class WithInjectedDependenciesJob : IJob
+        {
+            private readonly IDependencyInjected _dependencyInjected;
+
+            public WithInjectedDependenciesJob(IDependencyInjected dependencyInjected)
+            {
+                _dependencyInjected = dependencyInjected;
+            }
+
+            public void Execute(IJobExecutionContext context)
+            {
+                _dependencyInjected.Execute();
+            }
+        }
+
+        public interface IDependencyInjected
+        {
+            void Execute();
+        }
+
+        public class DependencyInjected : IDependencyInjected
+        {
+            public void Execute()
+            {
+                Console.WriteLine("[" + typeof(DependencyInjected).Name + "] Triggered " + DateTime.Now.ToLongTimeString());
+            }
+        }
+    }
+}

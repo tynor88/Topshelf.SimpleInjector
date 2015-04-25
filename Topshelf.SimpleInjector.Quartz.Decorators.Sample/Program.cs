2955210175291bcfee7421403d8222ba78a76ed6
Thomas Tynor <tynor@hotmail.com>
2015-04-25 17:41:40 +0200

    new sample project for dectorators with simple injector and quartz

---
+++ Topshelf.SimpleInjector.Quartz.Decorators.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topshelf.SimpleInjector.Quartz.Decorators.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

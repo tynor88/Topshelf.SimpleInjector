3b990400626c654bab14f42326cb2c4d55a37782
Thomas Tynor <tynor@hotmail.com>
2015-04-25 19:29:12 +0200

    upgrade version

--- Source/AssemblyVersion.cs
+++ Source/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.3.2")]
-[assembly: AssemblyFileVersion("0.1.3.2")]
+[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyFileVersion("0.1.4.0")]

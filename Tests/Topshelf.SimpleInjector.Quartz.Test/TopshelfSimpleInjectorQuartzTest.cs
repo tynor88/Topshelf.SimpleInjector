9434515ace657ae573036985afa9fd4532f7d958
Thomas Tynor <tynor@hotmail.com>
2015-12-27 14:24:55 +0100

    Revert "Revert "coverage""
    This reverts commit 7f07e9f30d0c60bc8e5a824fdf72c7cf608671df.

--- Tests/Topshelf.SimpleInjector.Quartz.Test/TopshelfSimpleInjectorQuartzTest.cs
+++ Tests/Topshelf.SimpleInjector.Quartz.Test/TopshelfSimpleInjectorQuartzTest.cs
@@ -205,31 +205,47 @@ namespace Topshelf.SimpleInjector.Quartz.Test
         }
 
         [Test, RunInApplicationDomain]
+        public void ExceptionIsThrownWhenIJobInstanceCannotBeResolvedFromContainerTest()
+        {
+            //Arrange
+            Container nullContainer = null;
+
+            //Act
+            var exception = Assert.Throws<ArgumentNullException>(() =>
+                HostFactory.New(config =>
+                {
+                    config.UseTestHost();
+                    config.UseQuartzSimpleInjector(nullContainer);
+                }));
+
+            //Assert
+            Assert.AreEqual("Value cannot be null.\r\nParameter name: container", exception.Message);
+        }
+
+        [Test, RunInApplicationDomain]
         public void ExceptionIsThrownWhenMissingCallToHostConfiguratorUseQuartzSimpleInjectorTest()
         {
             //Arrange
             Mock<IJob> testJobMock = new Mock<IJob>();
-            _container.RegisterSingleton<IJob>(() => testJobMock.Object);
+            _container.Register<ISampleDependency, SampleDependency>();
 
             //Act
-            var exception = Assert.Throws<ServiceBuilderException>(() =>
-                HostFactory.New(config =>
-                {
-                    config.UseTestHost();
-                    //config.UseQuartzSimpleInjector(_container); Missing
-                    _container.Verify();
-                    config.Service<TestService>(s =>
-                    {
-                        s.ScheduleQuartzJob(configurator => configurator.WithSimpleRepeatableSchedule<IJob>(TimeSpan.FromMilliseconds(1)));
+            var host = HostFactory.New(config =>
+            {
+                config.UseTestHost();
+                config.UseQuartzSimpleInjector(_container);
+                _container.Verify();
+                config.ScheduleQuartzJobAsService(configurator =>
+                    configurator
+                    .WithJob(() => JobBuilder.Create<IJob>().Build())
+                    .WithSimpleRepeatableSchedule<IJob>(TimeSpan.FromMilliseconds(1)));
+            });
 
-                        s.ConstructUsingSimpleInjector();
-                        s.WhenStarted((service, control) => service.Start());
-                        s.WhenStopped((service, control) => service.Stop());
-                    });
-                }));
+            var exitCode = host.Run();
 
             //Assert
-            Assert.AreEqual("An exception occurred creating the service: TestService", exception.Message);
+            Assert.AreEqual(TopshelfExitCode.Ok, exitCode);
+            testJobMock.Verify(job => job.Execute(It.IsAny<IJobExecutionContext>()), Times.Never);
         }
 
         [Test, RunInApplicationDomain]

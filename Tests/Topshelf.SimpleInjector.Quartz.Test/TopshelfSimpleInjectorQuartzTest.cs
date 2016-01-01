78b2b26cdc28ec77fbe343140fb3aea51d1c3cc4
Thomas Tynor <tynor@hotmail.com>
2016-01-01 18:47:00 +0100

    one more attempt

--- Tests/Topshelf.SimpleInjector.Quartz.Test/TopshelfSimpleInjectorQuartzTest.cs
+++ Tests/Topshelf.SimpleInjector.Quartz.Test/TopshelfSimpleInjectorQuartzTest.cs
@@ -302,28 +302,23 @@ namespace Topshelf.SimpleInjector.Quartz.Test
             //Act
             Exception exception = null;
 
-            HostFactory.New(config =>
+            try
             {
-                config.UseTestHost();
-                config.UseQuartzSimpleInjector(_container);
-                _container.Verify();
-                config.Service<TestService>(s =>
+                HostFactory.New(config =>
                 {
-                    try
-                    {
-                        s.ScheduleQuartzJob(configurator => configurator.WithCronSchedule<IJob>("Invalid Cron Schedule"));
-                    }
-                    catch (Exception ex)
-                    {
-                        exception = ex;
-                    }
-
-                    s.ConstructUsingSimpleInjector();
-                    s.WhenStarted((service, control) => service.Start());
-                    s.WhenStopped((service, control) => service.Stop());
+                    config.UseTestHost();
+                    config.UseQuartzSimpleInjector(_container);
+                    _container.Verify();
+                    config.ScheduleQuartzJobAsService(configurator =>
+                        configurator
+                            .WithJob(() => JobBuilder.Create<IJob>().Build())
+                            .WithCronSchedule<IJob>("Invalid Cron Schedule"));
                 });
-            });
-
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
 
             //Assert
             Assert.AreEqual("must specify a valid cron expression\r\nParameter name: cronExpression", exception.Message);
